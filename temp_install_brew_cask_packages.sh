a325eca30847314b0487d4c7b3242d846a05ab5a
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2025-04-20 00:04:03 -0400

    feat: add new role to install common apps

--- temp_install_brew_cask_packages.sh
+++
@@ -1,4 +0,0 @@
-brew install --cask google-chrome docker firefox slack obsidian amethyst caffeine 1password 1password-cli postman postico drawio iterm2
-# vagrant
-# virtualbox not rigt arch
-# visual-studio-code
